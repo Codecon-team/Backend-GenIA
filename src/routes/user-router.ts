2d1ee3fe82496dc585a69bcb38a9496115de7382
Eduardo Montandon <=>
2025-05-17 17:19:10 -0300

    feat: add getMeUser route and improve user creation logic

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { registerUserController, loginUserController } from "../controllers/user-controller";
+import { registerUserController, loginUserController, getMeUserController } from "../controllers/user-controller";
+import { checkToken } from "../utils/checkToken";
 
 const userRouter = Router()
 
 userRouter.post("/", registerUserController)
 userRouter.post("/login", loginUserController)
+userRouter.get("/me", checkToken, getMeUserController)
 
 
 export { userRouter }
--- src/service/user/create-user.ts
+++ src/service/user/create-user.ts
@@ -5,47 +5,49 @@ import { RegisterUser } from "../../types/user-type";
 import prisma from "../../prisma/client";
 
 export async function createUser(userDTO: RegisterUser) {
-    const existUser = await prisma.user.findUnique({
-        where: { email: userDTO.email }
-    });
-    const basicPlan = await prisma.plan.findUnique({
-        where: { slug: 'basic' }
-      })
+  const existUser = await prisma.user.findFirst({
+    where: {
+      OR: [{ username: userDTO.username }, { email: userDTO.email }],
+    },
+  });
+  const basicPlan = await prisma.plan.findUnique({
+    where: { slug: "basic" },
+  });
 
-    if (!basicPlan) {
-        logger.error("Basic plan not found");
-        throw new AppError("Basic plan not found", 500);
-    }    
+  if (!basicPlan) {
+    logger.error("Basic plan not found");
+    throw new AppError("Basic plan not found", 500);
+  }
 
-    if (existUser) {
-        logger.error("User already exists");
-        throw new AppError("User already exists", 400);
-    }
+  if (existUser) {
+    logger.error("User already exists");
+    throw new AppError("User already exists", 400);
+  }
 
-    const hashedPassword = await argon2.hash(userDTO.password);
+  const hashedPassword = await argon2.hash(userDTO.password);
 
-    const userData: RegisterUser = {
-        username: userDTO.username,
-        email: userDTO.email,
-        password: hashedPassword,
-    }
+  const userData: RegisterUser = {
+    username: userDTO.username,
+    email: userDTO.email,
+    password: hashedPassword,
+  };
 
-    const savedUser = await prisma.user.create({
-        data: {
-          ...userData,
-          subscriptions: {
-            create: {
-              plan_id: basicPlan.id,
-              status: 'active',
-              start_date: new Date(),
-              stripe_subscription_id: 'free-plan' 
-            }
-          }
+  const savedUser = await prisma.user.create({
+    data: {
+      ...userData,
+      subscriptions: {
+        create: {
+          plan_id: basicPlan.id,
+          status: "active",
+          start_date: new Date(),
+          stripe_subscription_id: "free-plan",
         },
-        include: {
-          subscriptions: true
-        }
-      })
+      },
+    },
+    include: {
+      subscriptions: true,
+    },
+  });
 
-    return savedUser;
+  return savedUser;
 }
