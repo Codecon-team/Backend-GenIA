67b078bc8f95ea1eea5442ea735f7daabe38f6ca
BlackVSK <phrod2007@gmail.com>
2025-05-18 02:42:27 -0300

    REFACTOR: atualizando codigo para escutar em todas as interfaces de rede e adicionando o cors

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,17 @@ import 'dotenv/config'
 import express from 'express';
 import { errorHandler } from './middlewares/error-handle';
 import routes from './routes';
+import cors from 'cors'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use('/api', routes)
 app.use(errorHandler)
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
     console.log(`🚀🚀🚀 Server is running on http://localhost:${PORT}`)
 })
 
